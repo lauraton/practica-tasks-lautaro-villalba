745ad85cf30f36bb2bb52b423d22ef8b44031768
lauraton <nickk5926@gmail.com>
2026-08-12 19:24:14 -0300

    Feat: se crearon las rutas de los endpoints

---
+++ src/routes/user.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import {
+    createUser,
+    getUsers,
+    getUserById,
+    updateUser,
+    deleteUser
+} from "../controllers/user.controller.js";
+
+export const userRouter = express.Router();
+
+userRouter.post("/users", createUser);
+userRouter.get("/users", getUsers);
+userRouter.get("/users/:id", getUserById);
+userRouter.put("/users/:id", updateUser);
+userRouter.delete("/users/:id", deleteUser);
